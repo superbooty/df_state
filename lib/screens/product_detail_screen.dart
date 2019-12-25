9d24028562d07b2ed6513ac26aafc06f7bc2dddf
Alfonso Guevara <aguevara1@levi.com>
2019-12-25 09:15:24 -0800

    adding waist/length selection

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,6 +16,8 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
+    final waist = 'Not Selected';
+    final length = 'Not Selected';
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
@@ -34,84 +36,118 @@ class ProductDetailScreen extends StatelessWidget {
                 builder: (ctx, details, _) => Column(
                   children: <Widget>[
                     Container(
-                      child: CarouselSlider(
-                        height: mediaQuery.size.height * .58,
-                        enlargeCenterPage: true,
-                        viewportFraction: 1.0,
-                        items: details.mobileGallery.map((image) {
-                          return Container(
-                            width: mediaQuery.size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 1.0),
-                            child: Image.network(image.url),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    Divider(
-                      color: Colors.grey[600],
-                    ),
-                    SingleChildScrollView(
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(),
-                        child: Container(
-                          height: mediaQuery.size.height * .25,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Row(
+                      height: mediaQuery.size.height * .76,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              child: CarouselSlider(
+                                height: mediaQuery.size.height * .58,
+                                enlargeCenterPage: true,
+                                viewportFraction: 1.0,
+                                items: details.mobileGallery.map((image) {
+                                  return Container(
+                                    width: mediaQuery.size.width,
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 1.0),
+                                    child: Image.network(image.url),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                            Divider(
+                              color: Colors.grey[600],
+                            ),
+                            Container(
+                              child: Column(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
                                 children: <Widget>[
-                                  Container(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      details.product.price.formattedValue,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.redAccent[700],
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Container(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          details.product.price.formattedValue,
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.redAccent[700],
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                  Container(
-                                    width: mediaQuery.size.width * .7,
-                                    alignment: Alignment.centerRight,
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      details.product.name,
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        color: Colors.grey[700],
-                                        fontWeight: FontWeight.w600,
+                                      Container(
+                                        width: mediaQuery.size.width * .7,
+                                        alignment: Alignment.centerRight,
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          details.product.name,
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.grey[500],
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                    ],
                                   ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Container(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text('Waist - $waist',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.grey[500],
+                                            fontWeight: FontWeight.w800,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text('Length - $length',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.grey[500],
+                                            fontWeight: FontWeight.w800,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
                                 ],
                               ),
-                              // Text(details.product.description),
-                              Align(
-                                alignment: Alignment.bottomCenter,
-                                child: ButtonTheme(
-                                  minWidth: mediaQuery.size.width * .95,
-                                  child: FlatButton(
-                                    color: Colors.redAccent.shade700,
-                                    textColor: Colors.white,
-                                    padding: EdgeInsets.all(15.0),
-                                    onPressed: () {
-                                      /*...*/
-                                    },
-                                    child: Text(
-                                      'Add to Cart',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ),
-                                ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: mediaQuery.size.height * .1, 
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: ButtonTheme(
+                          minWidth: mediaQuery.size.width * .95,
+                          child: FlatButton(
+                            color: Colors.redAccent.shade700,
+                            textColor: Colors.white,
+                            padding: EdgeInsets.all(15.0),
+                            onPressed: () {
+                              /*...*/
+                            },
+                            child: Text(
+                              'Add to Cart',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
                               ),
-                            ],
+                            ),
                           ),
                         ),
                       ),
